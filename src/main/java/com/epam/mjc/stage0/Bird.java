9ac1d1fe24cbfd9400960274f0a1f0af0f7b64b1
georgy <ganus1111@gmail.com>
2022-09-19 11:15:04 +0300

    add task solution

--- src/main/java/com/epam/mjc/stage0/Bird.java
+++ src/main/java/com/epam/mjc/stage0/Bird.java
@@ -1,5 +1,20 @@
 package com.epam.mjc.stage0;
 
-public class Bird {
+public class Bird extends Animal {
+    private final String color;
+    private final int numberOfPaws;
+    boolean hasFur;
+    public Bird(){
+        color = "blue";
+        numberOfPaws = 2;
+        hasFur = false;
+    }
 
+    @Override
+    public String getDescription() {
+        String isHasFur = this.hasFur ? "a" : "no";
+        String isPawOrPaws = this.numberOfPaws != 1 ? " paws" : " paw";
+        String res = "This animal is mostly " + this.color + ". It has " + this.numberOfPaws + isPawOrPaws + " and " + isHasFur+ " fur.";
+        return res + " Moreover, it has 2 wings and can fly.";
+    }
 }
--- src/main/java/com/epam/mjc/stage0/Dog.java
+++ src/main/java/com/epam/mjc/stage0/Dog.java
@@ -1,5 +1,20 @@
 package com.epam.mjc.stage0;
 
-public class Dog {
+public class Dog extends Animal {
+    private final String color;
+    private final int numberOfPaws;
+    private final boolean hasFur;
+    public Dog(){
+        color = "brown";
+        numberOfPaws = 4;
+        hasFur = true;
+    }
 
+    @Override
+    public String getDescription() {
+        super.getDescription();
+        String isHasFur = this.hasFur ? "a" : "no";
+        String isPawOrPaws = this.numberOfPaws != 1 ? " paws" : " paw";
+        return "This animal is mostly " + this.color + ". It has " + this.numberOfPaws + isPawOrPaws + " and " + isHasFur+ " fur.";
+    }
 }
